31ad2c397c73ab5533e3b980e0d2fa814f88ddb1
Eprahemi <fsownstarnew@gmail.com>
2026-07-23 00:05:58 +0300

    feat(__confirm_yn): accept yeah/nah/yep/nope/sure/ok and fix Ctrl+C
    Expanded accepted inputs:
    - yes: y, yes, yeah, yep, yup, ya, sure, ok, okay
    - no: n, no, nah, nope, nahh, nahhh, noo, nooo, naw,
          no way, nope, no thanks, no thank you, negative
    
    Fixed Ctrl+C: use read without -l so signal handler fires
    correctly. Check _reply via global variable.

--- configs/fish/functions/__confirm_yn.fish
+++ configs/fish/functions/__confirm_yn.fish
@@ -2,7 +2,7 @@ function __confirm_yn --description 'Prompt for y/n confirmation with Ctrl+C sup
     # Usage: __confirm_yn "  Apply changes? [Y/n]: " y
     #   argv[1] = prompt string
     #   argv[2] = default (y = default yes, n = default no)
-    # Returns: 0 = yes, 1 = no
+    # Returns: 0 = yes, 1 = no, 130 = Ctrl+C
     # Ctrl+C once = cancel (return 1), twice = kill calling function (return 130)
     set -l prompt "$argv[1]"
     set -l default "y"
@@ -14,6 +14,7 @@ function __confirm_yn --description 'Prompt for y/n confirmation with Ctrl+C sup
         set -g __cyn_cc (math $__cyn_cc + 1)
     end
     while true
+        set -g _reply ""
         read -l _reply -P "$prompt"
         if test $__cyn_cc -ge 2
             functions -e __cyn_int
@@ -35,11 +36,11 @@ function __confirm_yn --description 'Prompt for y/n confirmation with Ctrl+C sup
             else
                 return 1
             end
-        else if test "$_lc" = "y"; or test "$_lc" = "yes"
+        else if test "$_lc" = "y"; or test "$_lc" = "yes"; or test "$_lc" = "yeah"; or test "$_lc" = "yep"; or test "$_lc" = "yup"; or test "$_lc" = "ya"; or test "$_lc" = "sure"; or test "$_lc" = "ok"; or test "$_lc" = "okay"
             functions -e __cyn_int
             set -g __cyn_cc 0
             return 0
-        else if test "$_lc" = "n"; or test "$_lc" = "no"
+        else if test "$_lc" = "n"; or test "$_lc" = "no"; or test "$_lc" = "nah"; or test "$_lc" = "nope"; or test "$_lc" = "nahh"; or test "$_lc" = "nope"; or test "$_lc" = "noo"; or test "$_lc" = "nooo"; or test "$_lc" = "nahhh"; or test "$_lc" = "nahh"; or test "$_lc" = "naw"; or test "$_lc" = "no way"; or test "$_lc" = "nope"; or test "$_lc" = "no thanks"; or test "$_lc" = "no thank you"; or test "$_lc" = "negative"
             functions -e __cyn_int
             set -g __cyn_cc 0
             return 1
